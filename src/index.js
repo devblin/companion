f2743547833b1ae82e7e12ecca45254d30c54ea1
Tharun-b777 <tharunkumaar7@gmail.com>
2021-03-23 22:46:26 +0530

    Added Header,Footer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Navbar from './componenets/Homepage';
+import Footer from './componenets/Footer';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Navbar />
+		<Footer />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
