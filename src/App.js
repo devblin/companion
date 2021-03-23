639cdf0acc43537ec9f5217e0dac250ac08c9d8d
unknown <mansibisen354@gmail.com>
2021-03-23 18:14:56 +0530

    basic UI development

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import Chatbox from './components/chatbox/Chatbox';
-
+import "./App.css";
+import Chatbox from "./components/chatbox/Chatbox";
+import Resources from "./api/resources";
+import Phrases from "./api/phrases";
+import Videos from "./api/videos";
 function App() {
-  return (
-    <div className="App">
-      react
-      <Chatbox/>
-    </div>
-  );
+    return (
+        <div className="App">
+            react
+            <Chatbox />
+            <Resources />
+            <Phrases />
+            <Videos />
+        </div>
+    );
 }
 
 export default App;
