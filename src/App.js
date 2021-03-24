a2c0e7915ffbf4f0a4280b2035119cb53e3db213
Tharun-b777 <tharunkumaar7@gmai.com>
2021-03-24 20:54:16 +0530

    Rebased,added button for avigation Rebased,Added button for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import Chatbox from './components/chatbox/Chatbox';
+import "./App.css";
+import Chatbox from "./components/chatbox/Chatbox";
+import Navbar from "./components/Homepage";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      react
-      <Chatbox/>
+      <Router>
+        <Route path="/" exact component={Navbar} />
+        <Route path="/Chat" exact component={Chatbox} />
+      </Router>
+      <Footer />
     </div>
   );
 }
