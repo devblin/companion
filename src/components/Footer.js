0584955ad0fd8191fbbb3801c057e2a56d538a38
Tharun-b777 <tharunkumaar7@gmai.com>
2021-03-24 20:54:16 +0530

    Rebased,added button for avigation Rebased,Added button for navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,21 +3,21 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 const useStyles = makeStyles((theme) => ({
-  footer: {
-    padding: theme.spacing(1, 4),
-    marginTop: "auto",
-    textAlign: "center",
-  },
+    footer: {
+        padding: theme.spacing(1, 4),
+        marginTop: "auto",
+        textAlign: "center",
+    },
 }));
 
 export default function StickyFooter() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div>
-      <Box maxWidth="sm" className={classes.footer} variant="primary">
-        <Typography variant="body1">Made with ❤ </Typography>
-      </Box>
-    </div>
-  );
+    return (
+        <div>
+            <Box maxWidth="sm" className={classes.footer} variant="primary">
+                <Typography variant="body1">Footer Notes</Typography>
+            </Box>
+        </div>
+    );
 }
