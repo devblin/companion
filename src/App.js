6a7d7d50647fca0f1c7acf3767276148e2c63e2b
Tharun-b777 <tharunkumaar7@gmai.com>
2021-03-25 13:16:37 +0530

    fixed typos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Chatbox from "./components/chatbox/Chatbox";
-import Navbar from "./components/Homepage";
+import Homepage from "./components/Homepage";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" exact component={Navbar} />
+        <Route path="/" exact component={Homepage} />
         <Route path="/Chat" exact component={Chatbox} />
       </Router>
       <Footer />
