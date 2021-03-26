ed62e9babc1d911bde4090f2aa58cf7724c4ca1b
unknown <mansibisen354@gmail.com>
2021-03-26 10:32:44 +0530

    modification in homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,6 +3,7 @@ import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+
 const useStyles = makeStyles((theme) => ({
     main: {
         minHeight: "100vh",
@@ -19,6 +20,14 @@ const Homepage = () => {
                 <Container>
                     <br></br>
                     <h1>Companion</h1>
+                    <img alt="logo" src={require("../public/logo.png")}></img>
+                    <p>
+                        The more we learn about mental health, the better we can
+                        provide helpful support to those in our lives who
+                        struggle with mental health conditions.Join with us on
+                        chat bot to get help on your anxiety.
+                    </p>
+
                     <Button href="/Chat" variant="contained" color="secondary">
                         CHAT
                     </Button>
--- src/components/chatbox/Chatbox.js
+++ src/components/chatbox/Chatbox.js
@@ -1,27 +1,27 @@
-import React, {Component} from 'react'
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import SendIcon from '@material-ui/icons/Send';
-import IconButton from '@material-ui/core/IconButton';
-import { withStyles } from '@material-ui/core/styles';
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import SendIcon from "@material-ui/icons/Send";
+import IconButton from "@material-ui/core/IconButton";
+import { withStyles } from "@material-ui/core/styles";
 
-import {useStyles} from './style';
-import MessageArea from './MessageArea';
+import { useStyles } from "./style";
+import MessageArea from "./MessageArea";
 
 const pd = require("paralleldots");
 pd.apiKey = process.env.REACT_APP_PARALLELDOTS_API;
 
 class Chatbox extends Component {
     constructor(props) {
-        super(props)
-    
+        super(props);
+
         this.state = {
-            messages: []
-        }
+            messages: [],
+        };
 
         this.sendMessage = this.sendMessage.bind(this);
     }
@@ -30,34 +30,42 @@ class Chatbox extends Component {
         let messageBox = document.getElementById("text-box");
         let messageVal = messageBox.value;
 
-        if(messageVal) {
-            let newMessages = [...this.state.messages, {type:'You', message:messageVal}];
+        if (messageVal) {
+            let newMessages = [
+                ...this.state.messages,
+                { type: "You", message: messageVal },
+            ];
             this.setState({
-                messages: newMessages
-            })
+                messages: newMessages,
+            });
             messageBox.value = "";
 
-            pd.emotion(messageVal).then((res) => {
-                let emotions = JSON.parse(res).emotion;
-                console.log(emotions);
-                let maxEmotion = JSON.parse(this.getMaxEmotion(emotions));
+            pd.emotion(messageVal)
+                .then((res) => {
+                    let emotions = JSON.parse(res).emotion;
+                    console.log(emotions);
+                    let maxEmotion = JSON.parse(this.getMaxEmotion(emotions));
 
-                let botMsg = "Are you, " + maxEmotion.emotion;
+                    let botMsg = "Are you, " + maxEmotion.emotion;
 
-                newMessages = [...newMessages, {type:'Bot', message:botMsg}];
-                this.setState({
-                    messages: newMessages
+                    newMessages = [
+                        ...newMessages,
+                        { type: "Bot", message: botMsg },
+                    ];
+                    this.setState({
+                        messages: newMessages,
+                    });
                 })
-            }).catch((e) => {
-                console.log(e);
-            });
+                .catch((e) => {
+                    console.log(e);
+                });
         }
     }
 
     getMaxEmotion(emotions) {
-        let maxEmotion = { emotion:'', score:0 };
-        for(let e in emotions) {
-            if(emotions[e] > maxEmotion.score) {
+        let maxEmotion = { emotion: "", score: 0 };
+        for (let e in emotions) {
+            if (emotions[e] > maxEmotion.score) {
                 maxEmotion.score = emotions[e];
                 maxEmotion.emotion = e;
             }
@@ -66,35 +74,43 @@ class Chatbox extends Component {
         return JSON.stringify(maxEmotion);
     }
 
-    render(){
-        const {classes}=this.props;
+    render() {
+        const { classes } = this.props;
         return (
             <div className={classes.chatBoxContainer}>
                 <Grid container spacing={1} justify="center">
                     <Grid item xs={12} sm={9} md={6} lg={6}>
                         <Paper>
-                            <div className={classes.messageArea} style={{width:"100%"}}>
-                                <MessageArea messages={this.state.messages}/>
+                            <div
+                                className={classes.messageArea}
+                                style={{ width: "100%" }}
+                            >
+                                <MessageArea messages={this.state.messages} />
                             </div>
-                            <div style={{display:'flex', padding:10}}>
-                                <TextField 
-                                    id="text-box" 
-                                    placeholder="Enter Text" 
+                            <div style={{ display: "flex", padding: 10 }}>
+                                <TextField
+                                    id="text-box"
+                                    placeholder="Enter Text"
                                     margin="normal"
-                                    className ={classes.textBox}
+                                    className={classes.textBox}
                                     variant="outlined"
                                     fullWidth={true}
                                 />
-                                <IconButton onClick={this.sendMessage} aria-label='add' color='primary' type='submit'>
+                                <IconButton
+                                    onClick={this.sendMessage}
+                                    aria-label="add"
+                                    color="primary"
+                                    type="submit"
+                                >
                                     <SendIcon />
                                 </IconButton>
                             </div>
-                        </Paper>  
-                    </Grid>  
+                        </Paper>
+                    </Grid>
                 </Grid>
             </div>
-        )
+        );
     }
 }
 
-export default withStyles(useStyles)(Chatbox)
+export default withStyles(useStyles)(Chatbox);
